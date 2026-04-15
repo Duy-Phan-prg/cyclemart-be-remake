bcc763901f085c8e53cbb0264ad89923382b447e
Kin <tahoangnhat@gmail.com>
2026-04-15 08:30:11 +0700

    [FEAT]Validation khi đăng nhập và đăng kí

--- src/main/java/com/example/cyclemartberemake/entity/UserStatus.java
+++ src/main/java/com/example/cyclemartberemake/entity/UserStatus.java
@@ -1,5 +1,8 @@
 package com.example.cyclemartberemake.entity;
 
 public enum UserStatus {
-    ACTIVE, BANNED, SUSPENDED
+    ACTIVE,
+    BANNED,
+    SUSPENDED,
+    LOCKED  // Account is locked due to multiple failed login attempts
 }

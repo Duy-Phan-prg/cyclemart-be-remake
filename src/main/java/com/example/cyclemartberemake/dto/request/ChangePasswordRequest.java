6711ac3fccb322429ad264558b8d99546b4b4e67
Kin <tahoangnhat@gmail.com>
2026-04-22 22:38:17 +0700

    [FIX]thêm valid khi đổi password và valid khi updateprofile

--- src/main/java/com/example/cyclemartberemake/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/example/cyclemartberemake/dto/request/ChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package com.example.cyclemartberemake.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,6 +11,11 @@ public class ChangePasswordRequest {
     private String oldPassword;
 
     @NotBlank(message = "Mật khẩu mới không được để trống")
+    @Size(min = 8, message = "Password phải có ít nhất 8 ký tự")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).+$",
+            message = "Password phải có ít nhất 1 chữ hoa và 1 ký tự đặc biệt"
+    )
     private String newPassword;
 
     @NotBlank(message = "Xác nhận mật khẩu mới không được để trống")
